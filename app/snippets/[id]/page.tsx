ac5598c6539c23e76cf3ecce97ff6b71c86747a3
LeeTrent <lee.trent.1@gmail.com>
2024-03-28 08:33:28 -0500

    Added loading page

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -7,7 +7,7 @@ interface SnippetShowPageProps {
     }
 }
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
-    console.log("[SnippetShowPage] => (props):", props);
+    await new Promise( (r) => setTimeout(r, 2000) );
 
     const snippet = await db.snippet.findFirst({
         where: { id: parseInt(props.params.id) }
