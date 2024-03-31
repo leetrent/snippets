be41b42afab1474581dea45b078db885c2903e34
LeeTrent <lee.trent.1@gmail.com>
2024-03-30 21:12:59 -0500

    Added delete snippet functionality

--- actions/index.ts
+++ actions/index.ts
@@ -3,11 +3,16 @@ import { db } from '@/db';
 import { redirect } from 'next/navigation';
 
 export async function editSnippet(id: number, code: string) {
-    console.log("[actions][index.tx] => (id):", id);
-    console.log("[actions][index.tx] => (code):", code);
     await db.snippet.update({
         where: { id },
         data: { code}
     });
     redirect(`/snippets/${id}`);
+}
+
+export async function deleteSnippet(id: number) {
+    await db.snippet.delete({
+        where: { id }
+    });
+    redirect('/');
 }
