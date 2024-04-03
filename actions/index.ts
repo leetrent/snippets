e44d773dd93efcc480eb742f3650837735e08cd8
LeeTrent <lee.trent.1@gmail.com>
2024-04-03 09:08:47 -0500

    Added excetpion handling for create snippet

--- actions/index.ts
+++ actions/index.ts
@@ -18,23 +18,33 @@ export async function deleteSnippet(id: number) {
 }
 
 export async function createSnippet(formState: {message: string}, formData: FormData) {  
-    const title = formData.get('title');
-    const code = formData.get('code');
-
-    if (typeof title !== 'string' || title.length < 3) {
-        return {message: 'Title must be longer'};
-    }
-
-    if (typeof code !== 'string' || code.length < 10) {
-        return {message: 'Code must be longer'};
-    }
-
-    const snippet = await db.snippet.create({
-        data: {
-            title,
-            code
+    try {
+        const title = formData.get('title');
+        const code = formData.get('code');
+    
+        if (typeof title !== 'string' || title.length < 3) {
+            return {message: 'Title must be longer'};
         }
-    });
-    console.log(snippet);
+    
+        if (typeof code !== 'string' || code.length < 10) {
+            return {message: 'Code must be longer'};
+        }
+    
+        await db.snippet.create({
+            data: {
+                title,
+                code
+            }
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                message: err.message
+            };
+        } else {
+            message: "Something went wrong..."
+        }
+    }
+
     redirect('/');
 }
